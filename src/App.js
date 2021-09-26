54aca077408d5a385c742dda07e9e56967abf580
HR Williams <halrubinwilliams@gmail.com>
2021-09-26 14:47:18 -0700

    Successfully get token

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Dropdown from './Dropdown';
+import { Credentials } from './Credentials';
+import axios from "axios";
 
 const App = () => {
-  
+
+  const spotify = Credentials();
+
+  console.log('RENDERING APP.JS');
+
   const data = [
-    {value: 1, name: "A"},
-    {value: 2, name: "B"},
-    {value: 3, name: "C"},
-  ]
-  return(
-    <div>
-      <Dropdown options={data}/>
-    </div>
+    { value: 1, name: "A" },
+    { value: 2, name: "B" },
+    { value: 3, name: "C" },
+  ];
+
+  const[token, setToken] = useState('');
+
+  useEffect(() => {
+
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      console.log(tokenResponse.data.access_token);
+      setToken(tokenResponse.data.access_token);
+    });
+
+  }, []);
+
+  
+
+  return (
+    <form onSubmit={() => { }}>
+      <div className="container">
+        <Dropdown options={data} />
+        <Dropdown options={data} />
+        <button type='submit'>
+          Search
+        </button>
+      </div>
+    </form>
   );
 }
 
