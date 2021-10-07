1b41285046ac6cb19527dba58acfdd9f9c3999e4
HR Williams <halrubinwilliams@gmail.com>
2021-10-06 20:02:32 -0700

    Gets song features stores them in features with usestate

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
   const [playlist, setPlaylist] = useState({selectedPlaylist: '', listOfPlaylistFromAPI: []});
   const [tracks, setTracks] = useState({selectedTrack: '', listOfTracksFromAPI: []});
   const [trackDetail, setTrackDetail] = useState(null);
+  const [features, setFeatures] = useState({acousticness: '', danceability: '', energy: '', instrumentalness: '', liveness: '', speechiness: '', valence: ''});
   
   useEffect(() => {
 
@@ -36,7 +37,7 @@ const App = () => {
     .then(tokenResponse => {      
       setToken(tokenResponse.data.access_token);
 
-      axios('https://api.spotify.com/v1/browse/categories?locale=en_US', {
+      axios('https://api.spotify.com/v1/browse/categories', {
         method: 'GET',
         headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
       })
@@ -59,7 +60,7 @@ const App = () => {
       listOfGenresFromAPI: genres.listOfGenresFromAPI
     });
 
-    axios(`https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`, {
+    axios(`https://api.spotify.com/v1/browse/categories/${val}/playlists`, {
       method: 'GET',
       headers: { 'Authorization' : 'Bearer ' + token}
     })
@@ -86,7 +87,7 @@ const App = () => {
   const buttonClicked = e => {
     e.preventDefault();
 
-    axios(`https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`, {
+    axios(`https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks`, {
       method: 'GET',
       headers: {
         'Authorization' : 'Bearer ' + token
@@ -107,8 +108,30 @@ const App = () => {
     const trackInfo = currentTracks.filter(t => t.track.id === val);
 
     setTrackDetail(trackInfo[0].track);
+    console.log(val + " val")
+
+    axios(`https://api.spotify.com/v1/audio-features/${val}`, {
+      method: 'GET',
+      headers: {
+        'Authorization' : 'Bearer ' + token
+      }
+    })
+    .then(featuresResponse =>{
+      setFeatures({
+        acousticness: featuresResponse.data.acousticness,
+        dancebility: featuresResponse.data.dancebility,
+        energy: featuresResponse.data.energy,
+        instrumentalness: featuresResponse.data.instrumentalness,
+        liveness: featuresResponse.data.liveness,
+        speechiness: featuresResponse.data.speechiness,
+        valence: featuresResponse.data.valence
+        })
+      console.log(featuresResponse.data)
+  });
 
 console.log(val + " trackid")
+console.log(features.acousticness + " acousticness")
+// console.log(featuresResponse + " features")
 
   }
 
